f238266bf86f8bb3a89ded77e603c80a25d242cb
liuleigit <liulei_git@sina.com>
2017-03-22 23:47:01 +0800

    update

--- data.py
+++ data.py
@@ -5,6 +5,7 @@ real_dir_path = os.path.split(os.path.realpath(__file__))[0]
 data_path = os.path.join(real_dir_path, 'NewsFileCut')
 chanls = os.listdir(data_path)
 for ch in chanls:
+    print ch
     save_path = os.path.join(real_dir_path, 'data', ch)
     f = open(save_path, 'w')
     n = 0
@@ -12,7 +13,7 @@ for ch in chanls:
     files = os.listdir(ch_path)
     for ff in files:
         fr = open(os.path.join(ch_path, ff), 'r')
-        f.write(fr.read())
+        f.write(' '.join(fr.read().split()) + '\n')
 
     f.close()
 
