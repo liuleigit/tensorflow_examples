d6690a3cee3fe992c5c168aa26318fc989c9c9ac
liulei <liulei@lieying.cn>
2017-05-23 08:53:20 +0800

    update

--- data_process/gensim_word2vec.py
+++ data_process/gensim_word2vec.py
@@ -7,17 +7,21 @@
 import pandas as pd
 from gensim.models import Word2Vec
 #df = pd.read_csv('./data/data_cut/data_cut.csv')
-df = pd.read_csv('./data/data_cut/体育_cut.csv')
+df = pd.read_csv('./data/data_cut/test')
 sentences = df['doc']
 print type(sentences)
-#print sentences.tolist()[0]
+print sentences.tolist()[0]
 sentences = [i.decode('utf-8') for i in sentences.tolist()]
 
 model = Word2Vec(sentences, size=300, window=5, min_count=10, workers=10)
 model.save('./word2vec_model')
-model = Word2Vec.load('word2vec_model')
+print model.vocab
+#model = Word2Vec.load('word2vec_model')
+print model.wv[u'篮球']
+'''
 print model.similarity(u"刘国梁", u"张继科")
 print model.wv[u'张继科']
 print model.wv['张继科']
 print model.wv[u'篮板']
 print model.wv['篮板']
+'''
