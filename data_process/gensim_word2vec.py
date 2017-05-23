4bb55971538aabf06856f8166016b32ba0b5a1fd
liulei <liulei@lieying.cn>
2017-05-23 09:30:01 +0800

    update

--- data_process/gensim_word2vec.py
+++ data_process/gensim_word2vec.py
@@ -6,18 +6,20 @@
 # @Software: PyCharm Community Edition
 import pandas as pd
 from gensim.models import Word2Vec
+from gensim.models.word2vec import LineSentence
 #df = pd.read_csv('./data/data_cut/data_cut.csv')
-df = pd.read_csv('./data/data_cut/test')
-sentences = df['doc']
-print type(sentences)
-print sentences.tolist()[0]
-sentences = [i.decode('utf-8') for i in sentences.tolist()]
+#df = pd.read_csv('./data/data_cut/test')
+##sentences = df['doc']
+#print type(sentences)
+#print sentences.tolist()[0]
+#sentences = [i.decode('utf-8') for i in sentences.tolist()]
 
-model = Word2Vec(sentences, size=300, window=5, min_count=10, workers=10)
-model.save('./word2vec_model')
+model = Word2Vec(LineSentence('./data/data_cut/test'), size=300, window=5, min_count=10, workers=2)
+model.save('./word2vec.model')
 print model.vocab
+print type(model.vocab)
 #model = Word2Vec.load('word2vec_model')
-print model.wv[u'篮球']
+#print model.wv[u'篮球']
 '''
 print model.similarity(u"刘国梁", u"张继科")
 print model.wv[u'张继科']
